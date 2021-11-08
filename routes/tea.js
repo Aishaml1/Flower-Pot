f9ee648fc2f2a3bbb7d1075f8e071bee68ed256a
Aishaml1 <Aisha.lugg1@icloud.com>
2021-11-08 10:05:27 -0500

    reviews

--- routes/tea.js
+++ routes/tea.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import * as teaCtrl from '../controllers/tea.js'
 import { isLoggedIn } from '../middleware/middleware.js'
-import { displayDescription } from '../middleware/teas.js'
+
 
 const router = Router()
 
 // localhost:3000/tea
 router.get('/', teaCtrl.index)
+
 
 
 export {
